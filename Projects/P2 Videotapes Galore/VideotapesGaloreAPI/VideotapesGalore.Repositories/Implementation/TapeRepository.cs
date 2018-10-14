93e8972cf7ee290a233a108526d166ae34d98009
arnarfs15 <arnar-freyr@hotmail.com>
2018-10-14 17:24:03 +0000

    added tape repository functions

--- Projects/P2 Videotapes Galore/VideotapesGaloreAPI/VideotapesGalore.Repositories/Implementation/TapeRepository.cs
+++ Projects/P2 Videotapes Galore/VideotapesGaloreAPI/VideotapesGalore.Repositories/Implementation/TapeRepository.cs
@@ -1,9 +1,62 @@
+using System.Collections.Generic;
+using VideotapesGalore.Models.DTOs;
+using VideotapesGalore.Models.InputModels;
 using VideotapesGalore.Repositories.Interfaces;
 
 namespace VideotapesGalore.Repositories.Implementation
 {
     public class TapeRepository : ITapeRepository
     {
-        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Tape"></param>
+        /// <returns></returns>
+        public int CreateTape(TapeInputModel Tape)
+        {
+            throw new System.NotImplementedException();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Id"></param>
+        public void DeleteTape(int Id)
+        {
+            throw new System.NotImplementedException();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <param name="Tape"></param>
+        public void EditTape(int Id, TapeInputModel Tape)
+        {
+            throw new System.NotImplementedException();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public List<TapeDTO> GetAllTapes()
+        {
+            throw new System.NotImplementedException();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        public TapeDetailDTO GetTapeById(int Id)
+        {
+            throw new System.NotImplementedException();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public List<TapeDTO> GetTapesBorrowedAtDate()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }

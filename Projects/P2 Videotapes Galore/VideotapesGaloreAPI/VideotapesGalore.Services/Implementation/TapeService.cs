fd415480ded93261c8b4e80bf9bce254e4cdd47f
arnarfs15 <arnar-freyr@hotmail.com>
2018-10-14 17:17:54 +0000

    added tapeservice functions

--- Projects/P2 Videotapes Galore/VideotapesGaloreAPI/VideotapesGalore.Services/Implementation/TapeService.cs
+++ Projects/P2 Videotapes Galore/VideotapesGaloreAPI/VideotapesGalore.Services/Implementation/TapeService.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using VideotapesGalore.Services.Interfaces;
+using VideotapesGalore.Models.DTOs;
+using VideotapesGalore.Models.InputModels;
 
 namespace VideotapesGalore.Services.Implementations
 {
@@ -9,21 +12,34 @@ namespace VideotapesGalore.Services.Implementations
     /// </summary>
     public class TapeService : ITapeService
     {
-        /// <summary>
-        /// File to log exceptions in
-        /// </summary>
-        private const string logFilePath = "exceptionlog.txt";
-
-        /// <summary>
-        /// Logs error message of exception to logfile
-        /// </summary>
-        /// <param name="message">error message to log to logfile</param>
-        public void LogToFile(string message)
+        public int CreateTape(TapeInputModel Tape)
         {
-            using (var file = new StreamWriter(logFilePath, true))
-            {
-                file.WriteLine($"{DateTime.Now}: {message}\n");
-            }
+            throw new NotImplementedException();
+        }
+
+        public void DeleteTape(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EditTape(int Id, TapeInputModel Tape)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TapeDTO> GetAllTapes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TapeDetailDTO GetTapeById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TapeBorrowRecordDTO> GetTapeReportAtDate(DateTime LoanDate)
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- Projects/P2 Videotapes Galore/VideotapesGaloreAPI/VideotapesGalore.Services/Interfaces/ITapeService.cs
+++ Projects/P2 Videotapes Galore/VideotapesGaloreAPI/VideotapesGalore.Services/Interfaces/ITapeService.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Collections.Generic;
+using VideotapesGalore.Models.DTOs;
+using VideotapesGalore.Models.InputModels;
+
 namespace VideotapesGalore.Services.Interfaces
 {
     /// <summary>
@@ -6,9 +11,38 @@ namespace VideotapesGalore.Services.Interfaces
     public interface ITapeService
     {
         /// <summary>
-        /// Logs error message of exception to logfile
+        /// 
         /// </summary>
-        /// <param name="message">error message to log to logfile</param>
-         void LogToFile(string message);
+        /// <returns></returns>
+         List<TapeDTO> GetAllTapes();
+         /// <summary>
+         /// 
+         /// </summary>
+         /// <param name="Id"></param>
+         /// <returns></returns>
+         TapeDetailDTO GetTapeById(int Id);
+         /// <summary>
+         /// 
+         /// </summary>
+         /// <param name="Tape"></param>
+         /// <returns></returns>
+         int CreateTape(TapeInputModel Tape);
+         /// <summary>
+         /// 
+         /// </summary>
+         /// <param name="Id"></param>
+         /// <param name="Tape"></param>
+         void EditTape(int Id, TapeInputModel Tape);
+         /// <summary>
+         /// 
+         /// </summary>
+         /// <param name="Id"></param>
+         void DeleteTape(int Id);
+         /// <summary>
+         /// 
+         /// </summary>
+         /// <param name="LoanDate"></param>
+         /// <returns></returns>
+         List<TapeBorrowRecordDTO> GetTapeReportAtDate(DateTime LoanDate);
     }
 }

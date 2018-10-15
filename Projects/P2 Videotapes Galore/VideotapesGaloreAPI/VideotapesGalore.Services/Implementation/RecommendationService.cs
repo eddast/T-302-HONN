5ce65e10b745d321141982136cc2b9f839a28f54
arnarfs15 <arnar-freyr@hotmail.com>
2018-10-15 20:50:15 +0000

    added recommendation service functions

--- Projects/P2 Videotapes Galore/VideotapesGaloreAPI/VideotapesGalore.Services/Implementation/RecommendationService.cs
+++ Projects/P2 Videotapes Galore/VideotapesGaloreAPI/VideotapesGalore.Services/Implementation/RecommendationService.cs
@@ -1,9 +1,18 @@
+using VideotapesGalore.Models.DTOs;
 using VideotapesGalore.Services.Interfaces;
 
 namespace VideotapesGalore.Services.Implementation
 {
     public class RecommendationService : IRecommendationService
     {
-        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="UserId"></param>
+        /// <returns></returns>
+        public TapeDTO GetRecommendationForUser(int UserId)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
--- Projects/P2 Videotapes Galore/VideotapesGaloreAPI/VideotapesGalore.Services/Interfaces/IRecommendationService.cs
+++ Projects/P2 Videotapes Galore/VideotapesGaloreAPI/VideotapesGalore.Services/Interfaces/IRecommendationService.cs
@@ -1,7 +1,14 @@
+using VideotapesGalore.Models.DTOs;
+
 namespace VideotapesGalore.Services.Interfaces
 {
     public interface IRecommendationService
     {
-         
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="UserId"></param>
+        /// <returns></returns>
+         TapeDTO GetRecommendationForUser(int UserId);
     }
 }

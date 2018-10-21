e87c744f01da4784c226466a1ce205f3a3a7be8b
arnarfs15 <arnar-freyr@hotmail.com>
2018-10-21 20:59:24 +0000

    updated test script

--- Projects/P2 Videotapes Galore/VideotapesGaloreAPI/VideotapesGalore.Tests/test.sh
+++ Projects/P2 Videotapes Galore/VideotapesGaloreAPI/VideotapesGalore.Tests/test.sh
@@ -1,3 +1,7 @@
 dotnet build
-coverlet ./bin/Debug/netcoreapp2.1/VideotapesGalore.Tests.dll --target "dotnet" --targetargs "test --no-build" --format opencover --exclude "[*]VideotapesGalore.Repositories.*" --exclude "[*]VideotapesGalore.Models.*"
-reportgenerator -reports:coverage.opencover.xml -targetdir:./coverage
+coverlet ./bin/Debug/netcoreapp2.1/VideotapesGalore.Tests.dll --target "dotnet" --targetargs "test --no-build" --format opencover --include "[*]VideotapesGalore.Services.*"
+
+# Remove lines 5 - 18 from coverage
+sed -i -e '5,18d;' ./coverage.opencover.xml
+
+reportgenerator -reports:coverage.opencover.xml -targetdir:./coverage 

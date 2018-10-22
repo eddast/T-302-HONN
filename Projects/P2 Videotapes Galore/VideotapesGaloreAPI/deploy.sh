587f1f2ceff601677663f98b889fc53134c6cc7b
arnarfs15 <arnar-freyr@hotmail.com>
2018-10-22 23:10:51 +0000

    deploy script final iteration

--- Projects/P2 Videotapes Galore/VideotapesGaloreAPI/deploy.sh
+++ Projects/P2 Videotapes Galore/VideotapesGaloreAPI/deploy.sh
@@ -1,1 +1,11 @@
-ssh -i ./videotapes-galore-key.pem 35.176.20.49
+ssh -i ./videotapes-galore-key.pem ec2-user@35.176.20.49 << EOF
+    cd VideotapesGaloreAPI/VideotapesGaloreAPI
+    git pull
+    cd VideotapesGalore.WebApi
+    sudo fuser -k 1337/tcp
+    sudo rm -rf ./publish
+
+    sudo dotnet publish -o ./publish
+    cd publish/
+    sudo nohup dotnet VideotapesGalore.WebApi.dll --urls "http://*:1337" > ~/log.txt 2>&1 &
+EOF

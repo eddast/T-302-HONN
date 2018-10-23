d61e17c70cef074094c434e70a926d1f1abd0e98
arnarfs15 <arnar-freyr@hotmail.com>
2018-10-23 21:49:08 +0000

    started work on crud tape tests

--- Projects/P2 Videotapes Galore/VideotapesGaloreAPI/VideotapesGalore.IntegrationTests/TapeTests.cs
+++ Projects/P2 Videotapes Galore/VideotapesGaloreAPI/VideotapesGalore.IntegrationTests/TapeTests.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Net;
 using System;
+using System.Text;
 using System.Net.Http;
 using System.Threading.Tasks;
 using AngleSharp.Dom.Html;
@@ -11,6 +12,8 @@ using Xunit;
 using Xunit.Abstractions;
 using VideotapesGalore.WebApi;
 using VideotapesGalore.Models.InputModels;
+using VideotapesGalore.Models.DTOs;
+using Newtonsoft.Json;
 
 namespace VideotapesGalore.IntegrationTests
 {
@@ -26,31 +29,75 @@ namespace VideotapesGalore.IntegrationTests
         }
 
         [Fact]
-        public async Task SimulateCreateNewTape()
+        public async Task SimulateTapeCRUD()
         {
             /*** Urls to use in test ***/
-            string user = "api/v1/tapes";
+            string tapes = "api/v1/tapes";
 
-            // Arrange
+
             var client = _factory.CreateClient();
 
-            // Act
+            int previousLength = await GetCurrentTapesLength(client, tapes);
+            var currentDate = DateTime.Now;
+            /// [POST] api/v1/tape an invalid user model (name is required and email should be valid email)
+            var invalidTapeInput = new TapeInputModel(){
+                Title = "Mojo Jojo Attack on Townsville",
+                ReleaseDate = currentDate,
+                Type = "VHS",
+                EIDR = "10.5240/2B3B-1E0E-9314-2C6E-A453-3"
+            };
 
-            // Assert
+            var createPreconditionFailedResponse = await PostTape(client, tapes, invalidTapeInput);
+            Assert.Equal(HttpStatusCode.PreconditionFailed, createPreconditionFailedResponse.StatusCode);
+
+            /// [POST] api/v1/tape a valid user model (name is required and email should be valid email)
+            var tapeInput = new TapeInputModel(){
+                Title = "Mojo Jojo Attack on Townsville",
+                Director = "Not Mojo Jojo",
+                ReleaseDate = currentDate,
+                Type = "VHS",
+                EIDR = "10.5240/2B3B-1E0E-9314-2C6E-A453-3"
+            };
+
+            var createTapeResponse = await PostTape(client, tapes, tapeInput);
+
+            // Expect response to POST request to be 201 (created) and location header pointing to new resource
+            Assert.Equal("", await createTapeResponse.Content.ReadAsStringAsync());
+            createTapeResponse.EnsureSuccessStatusCode();
+
+            int afterCreateLength = await GetCurrentTapesLength(client, tapes);
+            Assert.Equal(previousLength + 1, afterCreateLength);
+
+            /// [GET] user by the pointer from location header for response to previous POST request and check if user values match
+            await AssertGetTapeById(client, createTapeResponse.Headers.Location, tapeInput);
         }
 
-        [Fact]
-        public async Task SimulateReviewTape()
+        private async Task<int> GetCurrentTapesLength(HttpClient client, string url) {
+            var response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            List<TapeDTO> tapes = JsonConvert.DeserializeObject<List<TapeDTO>>(await response.Content.ReadAsStringAsync());
+            return tapes.Count;
+        }
+
+        private async Task AssertGetTapeById(HttpClient client, Uri Location, TapeInputModel tapeInput)
         {
-            /*** Urls to use in test ***/
-            string user = "api/v1/tapes";
+            var validateCreateTapeResponse = await client.GetAsync(Location);
+            validateCreateTapeResponse.EnsureSuccessStatusCode();
+            TapeDTO newUser = JsonConvert.DeserializeObject<TapeDTO>(await validateCreateTapeResponse.Content.ReadAsStringAsync());
+            Assert.Equal(newUser.Title, tapeInput.Title);
+            Assert.Equal(newUser.Director, tapeInput.Director);
+            Assert.Equal(newUser.Type, tapeInput.Type);
+            Assert.Equal(newUser.ReleaseDate, tapeInput.ReleaseDate);
+            Assert.Equal(newUser.EIDR, tapeInput.EIDR);
+        }
 
-            // Arrange
-            var client = _factory.CreateClient();
+        private async Task<HttpResponseMessage> PostTape(HttpClient client, string url, TapeInputModel tapeInput)
+        {
+            var tapeInputJSON = JsonConvert.SerializeObject(tapeInput);
+            HttpContent content = new StringContent(tapeInputJSON, Encoding.UTF8, "application/json");
+            var createTapeResponse = await client.PostAsync(url, content);
+            return createTapeResponse;
 
-            // Act
-
-            // Assert
         }
     }
 }

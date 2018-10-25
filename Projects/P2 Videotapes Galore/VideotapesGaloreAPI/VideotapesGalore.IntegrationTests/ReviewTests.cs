cd2c4317debc42a2892f3a4c764496be154b024b
arnarfs15 <arnar-freyr@hotmail.com>
2018-10-25 20:44:31 +0000

    blind review tests

--- Projects/P2 Videotapes Galore/VideotapesGaloreAPI/VideotapesGalore.IntegrationTests/ReviewTests.cs
+++ Projects/P2 Videotapes Galore/VideotapesGaloreAPI/VideotapesGalore.IntegrationTests/ReviewTests.cs
@@ -37,65 +37,74 @@ namespace VideotapesGalore.IntegrationTests
         /// Also verify that proper errors are returned when user provides any bad or invalid input
         /// </summary>
         [Fact]
-        public async Task SimulateUserCRUD()
+        public async Task SimulateReviewCRUD()
         {
-            // Base URL to tape resources
-            string userBaseRoute = "api/v1/users";
+            // Base URL to review resources
+            string allReviewsRoute = "api/v1/tapes/reviews";
             var client = _factory.CreateClient();
 
-            /// [GET] get all users in system and store count
-            int allUsersCount = await GetCurrentUserCount(client, userBaseRoute);
-
-            /// [POST] attempt to create user using invalid user model (name is required and email should be valid email)
-            /// Expect response to be 412 (precondition failed) to indicate badly formatted input body from user
+            /***************** TODO EYÐA ÚT *****************/
             var userInput = new UserInputModel(){
-                Email = "Mojo Jojo",
-                Phone = "123 456 789",
-                Address = "Townsville"
-            };
-            var createFailResponse = await PostUser(client, userBaseRoute, userInput);
-            Assert.Equal(HttpStatusCode.PreconditionFailed, createFailResponse.StatusCode);
-
-            /// [POST] create new user using a valid user model
-            /// Expect response to POST request to be 201 (created) and expect to get location header pointing to new resource, then
-            /// [GET] user by the pointer from location header for response to previous POST request and check if user values match
-            userInput = new UserInputModel(){
-                Name = "Mojo Jojo",
-                Email = "m_jo@eviloverlords.com",
-                Phone = "123 456 789",
-                Address = "Townsville"
-            };
-            var createResponse = await PostUser(client, userBaseRoute, userInput);
-            Assert.Equal(HttpStatusCode.Created, createResponse.StatusCode);
-            var newResourceLocation = createResponse.Headers.Location;
-            await AssertGetUserById(client, newResourceLocation, userInput, true);
-
-            /// [GET] get all users in system and check that count has increased by one
-            Assert.Equal(allUsersCount+1, await GetCurrentUserCount(client, userBaseRoute));
-
-            // [PUT] attempt to update user using invalid input model (phone is required)
-            // Expect response to POST request to be 412 (for precondition failed)
-            // to indicate badly formatted input body from user
-            userInput = new UserInputModel(){
-                Name = "Mojo Jojo",
-                Email = "Mojo Jojo",
-                Address = "Townsville"
-            };
-            var editFailResponse = await PutUser(client, newResourceLocation, userInput);
-            Assert.Equal(HttpStatusCode.PreconditionFailed, editFailResponse.StatusCode);
-
-            /// [PUT] update user using a valid user model
-            /// Expect response to be 204 (no content) and then
-            /// [GET] user by id again and check if all values were updated in the put request
-            userInput = new UserInputModel(){
                 Name = "Venom",
                 Email = "mrv@symbiotefreaks.com",
                 Phone = "987 456 345",
                 Address = "New York"
             };
-            var editResponse = await PutUser(client, newResourceLocation, userInput);
+            var userInputJSON = JsonConvert.SerializeObject(userInput);
+            HttpContent usercontent = new StringContent(userInputJSON, Encoding.UTF8, "application/json");
+            var newUserRespone = await client.PostAsync("api/v1/users", usercontent);
+            var newUserLocation = newUserRespone.Headers.Location;
+
+            var tapeInput = new TapeInputModel() {
+                Title = "Mojo Jojo: Greatest Townsville Attacks",
+                Director = "Definately Not Mojo Jojo",
+                ReleaseDate = DateTime.Now,
+                Type = "VHS",
+                EIDR = "10.5240/2B3B-1E0E-9314-2C6E-A453-3"
+            };
+            var tapeInputJSON = JsonConvert.SerializeObject(tapeInput);
+            HttpContent tapecontent = new StringContent(userInputJSON, Encoding.UTF8, "application/json");
+            var newTapeRespone = await client.PostAsync("api/v1/users", tapecontent);
+            var newTapeLocation = newUserRespone.Headers.Location;
+            /************************************************/
+
+            /// [GET] get all reviews in system and store count
+            int allReviewsCount = await GetCurrentReviewsCount(client, allReviewsRoute);
+
+            /// [POST] attempt to create review using invalid review model (reveiw must range between 1-5)
+            /// Expect response to be 412 (precondition failed) to indicate badly formatted input body from user
+            var reviewInput = new ReviewInputModel(){ Rating = 0 };
+            // TODO OJJJJJJ
+            var createFailResponse = await PostReview(client, (newUserLocation + "/reviews/" + newTapeLocation.ToString()[newTapeLocation.ToString().Length - 1]), reviewInput);
+            Assert.Equal(HttpStatusCode.PreconditionFailed, createFailResponse.StatusCode);
+
+            /// [POST] create new user using a valid user model
+            /// Expect response to POST request to be 201 (created) and expect to get location header pointing to new resource, then
+            /// [GET] user by the pointer from location header for response to previous POST request and check if user values match
+            reviewInput = new ReviewInputModel(){ Rating = 3 };
+            // TODO OJJJJJ
+            var createResponse = await PostReview(client, (newUserLocation + "/reviews/" + newTapeLocation.ToString()[newTapeLocation.ToString().Length - 1]), reviewInput);
+            Assert.Equal(HttpStatusCode.Created, createResponse.StatusCode);
+            var newResourceLocation = createResponse.Headers.Location;
+            await AssertGetReview(client, newResourceLocation, reviewInput, true);
+
+            /// [GET] get all reviews in system and check that count has increased by one
+            Assert.Equal(allReviewsCount+1, await GetCurrentReviewsCount(client, allReviewsRoute));
+
+            // [PUT] attempt to update review using invalid input model (rating ranges from 1-5)
+            // Expect response to POST request to be 412 (for precondition failed)
+            // to indicate badly formatted input body from user
+            reviewInput = new ReviewInputModel(){ Rating = 6 };
+            var editFailResponse = await PutReview(client, newResourceLocation, reviewInput);
+            Assert.Equal(HttpStatusCode.PreconditionFailed, editFailResponse.StatusCode);
+
+            /// [PUT] update review using a valid user model
+            /// Expect response to be 204 (no content) and then
+            /// [GET] review by id again and check if all values were updated in the put request
+            reviewInput = new ReviewInputModel(){ Rating = 5 };
+            var editResponse = await PutReview(client, newResourceLocation, reviewInput);
             Assert.Equal(HttpStatusCode.NoContent, editResponse.StatusCode);
-            await AssertGetUserById(client, newResourceLocation, userInput, true);
+            await AssertGetReview(client, newResourceLocation, reviewInput, true);
 
             // [DELETE] new user by id and expect status to be 204 (no content)
             // Attempt to delete again and expect not found error (404)
@@ -104,7 +113,7 @@ namespace VideotapesGalore.IntegrationTests
             Assert.Equal(HttpStatusCode.NoContent, deleteResponse.StatusCode);
             var deleteFailResponse = await client.DeleteAsync(newResourceLocation);
             Assert.Equal(HttpStatusCode.NotFound, deleteFailResponse.StatusCode);
-            await AssertGetUserById(client, newResourceLocation, userInput, false);
+            await AssertGetReview(client, newResourceLocation, reviewInput, false);
         }
 
         /// <summary>
@@ -113,63 +122,60 @@ namespace VideotapesGalore.IntegrationTests
         /// <param name="client">http client to use to issue request to API</param>
         /// <param name="url">url to issue request to</param>
         /// <returns>count of all users in system</returns>
-        private async Task<int> GetCurrentUserCount(HttpClient client, string url) {
+        private async Task<int> GetCurrentReviewsCount(HttpClient client, string url) {
             var response = await client.GetAsync(url);
             response.EnsureSuccessStatusCode();
-            var users = JsonConvert.DeserializeObject<List<UserDTO>>(await response.Content.ReadAsStringAsync());
-            return users.Count;
+            var reviews = JsonConvert.DeserializeObject<List<ReviewDTO>>(await response.Content.ReadAsStringAsync());
+            return reviews.Count;
         }
 
         /// <summary>
-        /// Creates new user into the system e.g. conducts POST request
+        /// Creates new review into the system e.g. conducts POST request
         /// Returns response for post request
         /// </summary>
         /// <param name="client">http client to use to issue request to API</param>
         /// <param name="url">url to issue request to</param>
-        /// <param name="tapeInput">input model to use to create new tape</param>
+        /// <param name="reviewInput">input model to use to create new review</param>
         /// <returns></returns>
-        private async Task<HttpResponseMessage> PostUser(HttpClient client, string url, UserInputModel userInput)
+        private async Task<HttpResponseMessage> PostReview(HttpClient client, string url, ReviewInputModel reviewInput)
         {
-            var userInputJSON = JsonConvert.SerializeObject(userInput);
-            HttpContent content = new StringContent(userInputJSON, Encoding.UTF8, "application/json");
+            var reviewInputJSON = JsonConvert.SerializeObject(reviewInput);
+            HttpContent content = new StringContent(reviewInputJSON, Encoding.UTF8, "application/json");
             return await client.PostAsync(url, content);
         }
 
         /// <summary>
-        /// Updates existing user into the system e.g. conducts PUT request
+        /// Updates existing review into the system e.g. conducts PUT request
         /// Returns response for put request
         /// </summary>
         /// <param name="client">http client to use to issue request to API</param>
         /// <param name="url">url to issue request to</param>
-        /// <param name="userInput">input model to use to create new user</param>
+        /// <param name="reviewInput">input model to use to create new review</param>
         /// <returns>Response for HTTP request made</returns>
-        private async Task<HttpResponseMessage> PutUser(HttpClient client, Uri url, UserInputModel userInput)
+        private async Task<HttpResponseMessage> PutReview(HttpClient client, Uri url, ReviewInputModel reviewInput)
         {
-            var userInputJSON = JsonConvert.SerializeObject(userInput);
-            HttpContent content = new StringContent(userInputJSON, Encoding.UTF8, "application/json");
+            var reviewInputJSON = JsonConvert.SerializeObject(reviewInput);
+            HttpContent content = new StringContent(reviewInputJSON, Encoding.UTF8, "application/json");
             return await client.PutAsync(url, content);
         }
 
         /// <summary>
-        /// Fetches user by an id using Location URI (which we get when new user is created)
-        /// Either expect to get user back and verify that a given user matches user that is returned from request if shouldBeInSystem is set to true
+        /// Fetches user by an id using Location URI (which we get when new review is created)
+        /// Either expect to get review back and verify that a given review matches user that is returned from request if shouldBeInSystem is set to true
         /// Otherwise we assert that we get a not found error for resource
         /// </summary>
         /// <param name="client">http client to use to issue request to API</param>
         /// <param name="Location">uri to resource</param>
-        /// <param name="userInput">input model to compare to user we get back</param>
+        /// <param name="reviewInput">input model to compare to review we get back</param>
         /// <param name="shouldBeInSystem">true if we expect this resource to be in system, false if we expect 404 error</param>
         /// <returns>Response for HTTP request made</returns>
-        private async Task AssertGetUserById(HttpClient client, Uri Location, UserInputModel userInput, bool shouldBeInSystem)
+        private async Task AssertGetReview(HttpClient client, Uri Location, ReviewInputModel reviewInput, bool shouldBeInSystem)
         {
             var response = await client.GetAsync(Location);
             if(shouldBeInSystem) {
                 response.EnsureSuccessStatusCode();
-                UserDTO newUser = JsonConvert.DeserializeObject<UserDTO>(await response.Content.ReadAsStringAsync());
-                Assert.Equal(newUser.Name, userInput.Name);
-                Assert.Equal(newUser.Email, userInput.Email);
-                Assert.Equal(newUser.Phone, userInput.Phone);
-                Assert.Equal(newUser.Address, userInput.Address);
+                ReviewDTO newReview = JsonConvert.DeserializeObject<ReviewDTO>(await response.Content.ReadAsStringAsync());
+                Assert.Equal(newReview.Rating, reviewInput.Rating);
             } else {
                 Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
             }

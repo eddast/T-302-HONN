3d7f04fa4fd0427d027212ab59b28eafbdc34d3b
siminn-arnarfs <arnarfs@siminn.is>
2018-10-26 09:42:57 +0000

    fixed stylings

--- Projects/P2 Videotapes Galore/VideotapesGaloreAPI/VideotapesGalore.IntegrationTests/BorrowRecordTests.cs
+++ Projects/P2 Videotapes Galore/VideotapesGaloreAPI/VideotapesGalore.IntegrationTests/BorrowRecordTests.cs
@@ -18,133 +18,132 @@ using Newtonsoft.Json;
 
 namespace VideotapesGalore.IntegrationTests
 {
-  [Collection("Test Context Collection")]
-  public class BorrowRecordTests
-  {
-    private readonly WebApplicationFactory<Startup> _factory;
-    private TestsContextFixture _fixture;
-    private ITestOutputHelper output;
-
-    /// <summary>
-    /// Setup web application context as factory
-    /// </summary>
-    /// <param name="factory">the web application context</param>
-    public BorrowRecordTests(TestsContextFixture fixture, ITestOutputHelper output)
+    [Collection("Test Context Collection")]
+    public class BorrowRecordTests
     {
+        private readonly WebApplicationFactory<Startup> _factory;
+        private TestsContextFixture _fixture;
+        private ITestOutputHelper output;
 
-      _factory = fixture.factory;
-      _fixture = fixture;
-      this.output = output;
+        /// <summary>
+        /// Setup web application context as factory
+        /// </summary>
+        /// <param name="factory">the web application context</param>
+        public BorrowRecordTests(TestsContextFixture fixture, ITestOutputHelper output)
+        {
+
+            _factory = fixture.factory;
+            _fixture = fixture;
+            this.output = output;
+        }
+
+        /// <summary>
+        /// Simulates and tests all basic CRUD functionalities for Tape resource in system
+        /// E.g. adding tape resources, updating tape resources, deleteting tape resources and reading tape resources accordingly
+        /// Also verify that proper errors are returned when user provides any bad or invalid input
+        /// </summary>
+        [Fact]
+        public async Task SimulateTapeCRUD()
+        {
+            var tapeId = _fixture.tapeIds[0];
+            var userLocation = _fixture.userUrls[0];
+            var client = _factory.CreateClient();
+            var borrowRecordUrl = GetRecordPath(userLocation, tapeId);
+
+            var createRecordResponse = await CreateBorrowRecord(client, borrowRecordUrl);
+            Assert.Equal(HttpStatusCode.Created, createRecordResponse.StatusCode);
+            var userReviewCount = (await GetUserBorrowRecords(client, userLocation)).History.Count();
+            Assert.Equal(1, userReviewCount);
+
+            // Return the newly created tape
+            var returnTapeResponse = await ReturnTape(client, borrowRecordUrl);
+            Assert.Equal(HttpStatusCode.NoContent, returnTapeResponse.StatusCode);
+
+            // Get all borrow records again and verify that the return date has been updated
+            var allReviews = (await GetUserBorrowRecords(client, userLocation)).History.ToList();
+            Assert.Equal(1, allReviews.Count);
+            Assert.NotNull(allReviews[0].ReturnDate);
+
+            // Try to return tape again to verify that an error occurs
+            returnTapeResponse = await ReturnTape(client, borrowRecordUrl);
+            Assert.Equal(HttpStatusCode.NotFound, returnTapeResponse.StatusCode);
+
+            var updateModel = new BorrowRecordInputModel()
+            {
+              BorrowDate = new DateTime(0),
+              ReturnDate = DateTime.Now
+            };
+            var updateReviewResponse = await UpdateBorrowRecord(client, borrowRecordUrl, updateModel);
+            Assert.Equal(HttpStatusCode.NoContent, updateReviewResponse.StatusCode);
+
+            var updatedReviews = (await GetUserBorrowRecords(client, userLocation)).History.ToList();
+            Assert.Equal(1, updatedReviews.Count);
+            Assert.NotEqual(allReviews[0].ReturnDate, updatedReviews[0].ReturnDate);
+        }
+
+        /// <summary>
+        /// Creates new tape into the system e.g. conducts POST request
+        /// Returns response for post request
+        /// </summary>
+        /// <param name="client">http client to use to issue request to API</param>
+        /// <param name="url">url to issue request to</param>
+        /// <param name="tapeInput">input model to use to create new tape</param>
+        /// <returns></returns>
+        private async Task<HttpResponseMessage> PostTape(HttpClient client, string url, TapeInputModel tapeInput)
+        {
+            var tapeInputJSON = JsonConvert.SerializeObject(tapeInput);
+            HttpContent content = new StringContent(tapeInputJSON, Encoding.UTF8, "application/json");
+            return await client.PostAsync(url, content);
+        }
+
+        /// <summary>
+        /// Creates new user into the system e.g. conducts POST request
+        /// Returns response for post request
+        /// </summary>
+        /// <param name="client">http client to use to issue request to API</param>
+        /// <param name="url">url to issue request to</param>
+        /// <param name="tapeInput">input model to use to create new tape</param>
+        /// <returns></returns>
+        private async Task<HttpResponseMessage> PostUser(HttpClient client, string url, UserInputModel userInput)
+        {
+            var userInputJSON = JsonConvert.SerializeObject(userInput);
+            HttpContent content = new StringContent(userInputJSON, Encoding.UTF8, "application/json");
+            return await client.PostAsync(url, content);
+        }
+
+        private async Task<HttpResponseMessage> CreateBorrowRecord(HttpClient client, string url)
+        {
+            return await client.PostAsync(url, null);
+        }
+
+        private async Task<HttpResponseMessage> GetUrl(HttpClient client, string url)
+        {
+            return await client.GetAsync(url);
+        }
+
+        private async Task<HttpResponseMessage> UpdateBorrowRecord(HttpClient client, string url, BorrowRecordInputModel input)
+        {
+            var recordInputJson = JsonConvert.SerializeObject(input);
+            HttpContent content = new StringContent(recordInputJson, Encoding.UTF8, "application/json");
+            return await client.PutAsync(url, content);
+        }
+
+        private async Task<HttpResponseMessage> ReturnTape(HttpClient client, string url)
+        {
+            return await client.DeleteAsync(url);
+        }
+
+        private async Task<UserDetailDTO> GetUserBorrowRecords(HttpClient client, string url)
+        {
+            var getUserReviewsResponse = await GetUrl(client, url);
+            Assert.Equal(HttpStatusCode.OK, getUserReviewsResponse.StatusCode);
+            var borrowRecords = JsonConvert.DeserializeObject<UserDetailDTO>(await getUserReviewsResponse.Content.ReadAsStringAsync());
+            return borrowRecords;
+        }
+
+        private string GetRecordPath(string userpath, int tapeId)
+        {
+            return userpath + "/tapes/" + tapeId;
+        }
     }
-
-    /// <summary>
-    /// Simulates and tests all basic CRUD functionalities for Tape resource in system
-    /// E.g. adding tape resources, updating tape resources, deleteting tape resources and reading tape resources accordingly
-    /// Also verify that proper errors are returned when user provides any bad or invalid input
-    /// </summary>
-    [Fact]
-    public async Task SimulateTapeCRUD()
-    {
-      var tapeId = _fixture.tapeIds[0];
-      var userLocation = _fixture.userUrls[0];
-      var client = _factory.CreateClient();
-      var borrowRecordUrl = GetRecordPath(userLocation, tapeId);
-
-      var createRecordResponse = await CreateBorrowRecord(client, borrowRecordUrl);
-      Assert.Equal(HttpStatusCode.Created, createRecordResponse.StatusCode);
-      var userReviewCount = (await GetUserBorrowRecords(client, userLocation)).History.Count();
-      Assert.Equal(1, userReviewCount);
-
-      // Return the newly created tape
-      var returnTapeResponse = await ReturnTape(client, borrowRecordUrl);
-      Assert.Equal(HttpStatusCode.NoContent, returnTapeResponse.StatusCode);
-
-      // Get all borrow records again and verify that the return date has been updated
-      var allReviews = (await GetUserBorrowRecords(client, userLocation)).History.ToList();
-      Assert.Equal(1, allReviews.Count);
-      Assert.NotNull(allReviews[0].ReturnDate);
-
-      // Try to return tape again to verify that an error occurs
-      returnTapeResponse = await ReturnTape(client, borrowRecordUrl);
-      Assert.Equal(HttpStatusCode.NotFound, returnTapeResponse.StatusCode);
-
-      var updateModel = new BorrowRecordInputModel()
-      {
-        BorrowDate = new DateTime(0),
-        ReturnDate = DateTime.Now
-      };
-      var updateReviewResponse = await UpdateBorrowRecord(client, borrowRecordUrl, updateModel);
-      Assert.Equal(HttpStatusCode.NoContent, updateReviewResponse.StatusCode);
-
-      var updatedReviews = (await GetUserBorrowRecords(client, userLocation)).History.ToList();
-      Assert.Equal(1, updatedReviews.Count);
-      Assert.NotEqual(allReviews[0].ReturnDate, updatedReviews[0].ReturnDate);
-    }
-
-    /// <summary>
-    /// Creates new tape into the system e.g. conducts POST request
-    /// Returns response for post request
-    /// </summary>
-    /// <param name="client">http client to use to issue request to API</param>
-    /// <param name="url">url to issue request to</param>
-    /// <param name="tapeInput">input model to use to create new tape</param>
-    /// <returns></returns>
-    private async Task<HttpResponseMessage> PostTape(HttpClient client, string url, TapeInputModel tapeInput)
-    {
-      var tapeInputJSON = JsonConvert.SerializeObject(tapeInput);
-      HttpContent content = new StringContent(tapeInputJSON, Encoding.UTF8, "application/json");
-      return await client.PostAsync(url, content);
-    }
-
-    /// <summary>
-    /// Creates new user into the system e.g. conducts POST request
-    /// Returns response for post request
-    /// </summary>
-    /// <param name="client">http client to use to issue request to API</param>
-    /// <param name="url">url to issue request to</param>
-    /// <param name="tapeInput">input model to use to create new tape</param>
-    /// <returns></returns>
-    private async Task<HttpResponseMessage> PostUser(HttpClient client, string url, UserInputModel userInput)
-    {
-      var userInputJSON = JsonConvert.SerializeObject(userInput);
-      HttpContent content = new StringContent(userInputJSON, Encoding.UTF8, "application/json");
-      return await client.PostAsync(url, content);
-    }
-
-    private async Task<HttpResponseMessage> CreateBorrowRecord(HttpClient client, string url)
-    {
-      return await client.PostAsync(url, null);
-    }
-
-    private async Task<HttpResponseMessage> GetUrl(HttpClient client, string url)
-    {
-      return await client.GetAsync(url);
-    }
-
-    private async Task<HttpResponseMessage> UpdateBorrowRecord(HttpClient client, string url, BorrowRecordInputModel input)
-    {
-      var recordInputJson = JsonConvert.SerializeObject(input);
-      HttpContent content = new StringContent(recordInputJson, Encoding.UTF8, "application/json");
-      return await client.PutAsync(url, content);
-    }
-
-    private async Task<HttpResponseMessage> ReturnTape(HttpClient client, string url)
-    {
-      return await client.DeleteAsync(url);
-    }
-
-    private async Task<UserDetailDTO> GetUserBorrowRecords(HttpClient client, string url)
-    {
-      var getUserReviewsResponse = await GetUrl(client, url);
-      Assert.Equal(HttpStatusCode.OK, getUserReviewsResponse.StatusCode);
-      var borrowRecords = JsonConvert.DeserializeObject<UserDetailDTO>(await getUserReviewsResponse.Content.ReadAsStringAsync());
-      return borrowRecords;
-    }
-
-
-    private string GetRecordPath(string userpath, int tapeId)
-    {
-      return userpath + "/tapes/" + tapeId;
-    }
-  }
 }
--- Projects/P2 Videotapes Galore/VideotapesGaloreAPI/VideotapesGalore.IntegrationTests/TestsContextFixture.cs
+++ Projects/P2 Videotapes Galore/VideotapesGaloreAPI/VideotapesGalore.IntegrationTests/TestsContextFixture.cs
@@ -20,129 +20,124 @@ using System.Text;
 
 namespace VideotapesGalore.IntegrationTests
 {
-  public class TestsContextFixture : IAsyncLifetime
-  {
-    public WebApplicationFactory<Startup> factory { get; }
-    public HttpClient client { get; }
-    public List<int> tapeIds { get; set; }
-    public List<string> tapeUrls { get; set; }
-    public List<int> userIds { get; set; }
-    public List<string> userUrls { get; set; }
-    public TestsContextFixture()
+    public class TestsContextFixture : IAsyncLifetime
     {
-      this.factory = new WebApplicationFactory<Startup>();
-      client = this.factory.CreateClient();
-      this.tapeIds = new List<int>();
-      this.userIds = new List<int>();
-      this.tapeUrls = new List<string>();
-      this.userUrls = new List<string>();
-    }
-    public void Dispose()
-    {
-      RemoveFromDBAfterTests();
-    }
+        public WebApplicationFactory<Startup> factory { get; }
+        public HttpClient client { get; }
+        public List<int> tapeIds { get; set; }
+        public List<string> tapeUrls { get; set; }
+        public List<int> userIds { get; set; }
+        public List<string> userUrls { get; set; }
+        public TestsContextFixture()
+        {
+            this.factory = new WebApplicationFactory<Startup>();
+            client = this.factory.CreateClient();
+            this.tapeIds = new List<int>();
+            this.userIds = new List<int>();
+            this.tapeUrls = new List<string>();
+            this.userUrls = new List<string>();
+        }
 
+        public async Task InitializeAsync()
+        {
+            await InitializeDbForTests();
+        }
 
-    public async Task InitializeAsync()
-    {
-      await InitializeDbForTests();
-    }
+        public Task DisposeAsync()
+        {
+            RemoveFromDBAfterTests();
+            return Task.CompletedTask;
+        }
 
-    public Task DisposeAsync()
-    {
-      RemoveFromDBAfterTests();
-      return Task.CompletedTask;
-    }
-
-    public async Task InitializeDbForTests()
-    {
-      foreach (var user in GetSeedingUsers())
-      {
-        var userInputJson = JsonConvert.SerializeObject(user);
-        HttpContent content = new StringContent(userInputJson, Encoding.UTF8, "application/json");
-        var response = await client.PostAsync("/api/v1/users", content);
-        var path = response.Headers.Location.LocalPath;
-        userUrls.Add(path);
-        userIds.Add(Convert.ToInt32(path.Substring(path.LastIndexOf("/") + 1)));
-      }
-      foreach (var tape in GetSeedingTapes())
-      {
-        var tapeInputJson = JsonConvert.SerializeObject(tape);
-        HttpContent content = new StringContent(tapeInputJson, Encoding.UTF8, "application/json");
-        var response = await client.PostAsync("/api/v1/tapes", content);
-        var path = response.Headers.Location.LocalPath;
-        tapeUrls.Add(path);
-        tapeIds.Add(Convert.ToInt32(path.Substring(path.LastIndexOf("/") + 1)));
-      }
-    }
-
-    public void RemoveFromDBAfterTests()
-    {
-      deleteFromDb(userUrls);
-      deleteFromDb(tapeUrls);
-    }
-
-    private void deleteFromDb(List<string> urls)
-    {
-      foreach (var url in urls)
-      {
-        client.DeleteAsync(url);
-      }
-    }
-
-    public List<UserInputModel> GetSeedingUsers()
-    {
-      return new List<UserInputModel>()
+        public async Task InitializeDbForTests()
+        {
+            foreach (var user in GetSeedingUsers())
             {
-                new UserInputModel(){
-                    Name = "Eddy",
-                    Email = "eddy@genious.com",
-                    Address = "Mom's house",
-                    Phone = "8451234"
-                },
-                new UserInputModel(){
-                    Name = "Mojo Jojo",
-                    Email = "major@townsville.org",
-                    Address = "Townstreet 3",
-                    Phone = "8443511"
-                 },
-                new UserInputModel() {
-                    Name = "Johnny Bravo",
-                    Email = "hey-pretty-mama@hotmale.com",
-                    Address = "Street 123",
-                    Phone = "8227979"
-
-                }
-            };
-    }
-
-    public List<TapeInputModel> GetSeedingTapes()
-    {
-      return new List<TapeInputModel>()
+              var userInputJson = JsonConvert.SerializeObject(user);
+              HttpContent content = new StringContent(userInputJson, Encoding.UTF8, "application/json");
+              var response = await client.PostAsync("/api/v1/users", content);
+              var path = response.Headers.Location.LocalPath;
+              userUrls.Add(path);
+              userIds.Add(Convert.ToInt32(path.Substring(path.LastIndexOf("/") + 1)));
+            }
+            foreach (var tape in GetSeedingTapes())
             {
-                new TapeInputModel(){
-                    Title = "Mojo Jojo's Revenge",
-                    Director = "Mojo Jojo",
-                    ReleaseDate = DateTime.Now,
-                    Type = "VHS",
-                    EIDR = "10.5240/72B3-2D9E-35E1-6760-83FA-K"
-                },
-                new TapeInputModel(){
-                    Title = "Mojo Jojo's Revenge",
-                    Director = "Mojo Jojo",
-                    ReleaseDate = DateTime.Now,
-                    Type = "VHS",
-                    EIDR = "10.5240/72B3-2D9E-35E1-6760-83FA-K"
-                 },
-                new TapeInputModel() {
-                    Title = "Mojo Jojo's Revenge",
-                    Director = "Mojo Jojo",
-                    ReleaseDate = DateTime.Now,
-                    Type = "VHS",
-                    EIDR = "10.5240/72B3-2D9E-35E1-6760-83FA-K"
-                }
-            };
-    }
+              var tapeInputJson = JsonConvert.SerializeObject(tape);
+              HttpContent content = new StringContent(tapeInputJson, Encoding.UTF8, "application/json");
+              var response = await client.PostAsync("/api/v1/tapes", content);
+              var path = response.Headers.Location.LocalPath;
+              tapeUrls.Add(path);
+              tapeIds.Add(Convert.ToInt32(path.Substring(path.LastIndexOf("/") + 1)));
+            }
+        }
 
-  }
+        public void RemoveFromDBAfterTests()
+        {
+            deleteFromDb(userUrls);
+            deleteFromDb(tapeUrls);
+        }
+
+        private void deleteFromDb(List<string> urls)
+        {
+            foreach (var url in urls)
+            {
+              client.DeleteAsync(url);
+            }
+        }
+
+        public List<UserInputModel> GetSeedingUsers()
+        {
+          return new List<UserInputModel>()
+                {
+                    new UserInputModel(){
+                        Name = "Eddy",
+                        Email = "eddy@genious.com",
+                        Address = "Mom's house",
+                        Phone = "8451234"
+                    },
+                    new UserInputModel(){
+                        Name = "Mojo Jojo",
+                        Email = "major@townsville.org",
+                        Address = "Townstreet 3",
+                        Phone = "8443511"
+                    },
+                    new UserInputModel() {
+                        Name = "Johnny Bravo",
+                        Email = "hey-pretty-mama@hotmale.com",
+                        Address = "Street 123",
+                        Phone = "8227979"
+
+                    }
+                };
+        }
+
+        public List<TapeInputModel> GetSeedingTapes()
+        {
+          return new List<TapeInputModel>()
+                {
+                    new TapeInputModel(){
+                        Title = "Mojo Jojo's Revenge",
+                        Director = "Mojo Jojo",
+                        ReleaseDate = DateTime.Now,
+                        Type = "VHS",
+                        EIDR = "10.5240/72B3-2D9E-35E1-6760-83FA-K"
+                    },
+                    new TapeInputModel(){
+                        Title = "Mojo Jojo's Revenge",
+                        Director = "Mojo Jojo",
+                        ReleaseDate = DateTime.Now,
+                        Type = "VHS",
+                        EIDR = "10.5240/72B3-2D9E-35E1-6760-83FA-K"
+                    },
+                    new TapeInputModel() {
+                        Title = "Mojo Jojo's Revenge",
+                        Director = "Mojo Jojo",
+                        ReleaseDate = DateTime.Now,
+                        Type = "VHS",
+                        EIDR = "10.5240/72B3-2D9E-35E1-6760-83FA-K"
+                    }
+                };
+        }
+
+    }
 }

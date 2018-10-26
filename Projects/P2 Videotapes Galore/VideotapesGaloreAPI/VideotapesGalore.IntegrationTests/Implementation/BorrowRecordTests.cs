5abbba91ef5c5061d49d9b3014e30ad37c3cc5a8
eddast <eddasr15@ru.is>
2018-10-26 18:13:56 +0000

    refactor borrow tests

--- Projects/P2 Videotapes Galore/VideotapesGaloreAPI/VideotapesGalore.IntegrationTests/Implementation/BorrowRecordTests.cs
+++ Projects/P2 Videotapes Galore/VideotapesGaloreAPI/VideotapesGalore.IntegrationTests/Implementation/BorrowRecordTests.cs
@@ -22,130 +22,110 @@ namespace VideotapesGalore.IntegrationTests.Implementation
     [Collection("Test Context Collection")]
     public class BorrowRecordTests
     {
+        /// <summary>
+        /// Startup factory for test context
+        /// </summary>
         private readonly WebApplicationFactory<Startup> _factory;
+
+        /// <summary>
+        /// Setup test environment (along with seeded content)
+        /// </summary>
         private TestsContextFixture _fixture;
-        private ITestOutputHelper output;
+
+        /// <summary>
+        /// HTTP client to use
+        /// </summary>
+        protected HttpClient client;
 
         /// <summary>
         /// Setup web application context as factory
         /// </summary>
         /// <param name="factory">the web application context</param>
-        public BorrowRecordTests(TestsContextFixture fixture, ITestOutputHelper output)
+        public BorrowRecordTests(TestsContextFixture fixture)
         {
 
-            _factory = fixture.factory;
-            _fixture = fixture;
-            this.output = output;
+            this._factory = fixture.factory;
+            this._fixture = fixture;
+            this.client = fixture.factory.CreateClient();
         }
 
         /// <summary>
-        /// Simulates and tests all basic CRUD functionalities for Tape resource in system
-        /// E.g. adding tape resources, updating tape resources, deleteting tape resources and reading tape resources accordingly
+        /// Simulates and tests all basic functionalities for the relations for user and tape resources in system e.g. borrow records
+        /// And simulates possible actions (registering tape on loan, returning a tape for user, update borrow record (admin functionality) and reading records)
         /// Also verify that proper errors are returned when user provides any bad or invalid input
         /// </summary>
         [Fact]
-        public async Task SimulateBorrowRecordCRUD()
+        public async Task SimulateBorrowRecordActions()
         {
-            var tapeId = _fixture.tapeIds[1];
-            var userLocation = _fixture.userUrls[2];
-            var client = _factory.CreateClient();
-            var borrowRecordUrl = GetRecordPath(userLocation, tapeId);
+            // Use seeded content to aid tests, a single tape and single user from initialized content
+            // Seeded user no 2 has no borrow records and seeded tape no 1 is available (e.g. not on loan) so use that content
+            // Construct base URL to request borrow relation for selected seeded content
+            int tapeNo = 1, userNo = 2;
+            var tapeId = _fixture.tapeIds[tapeNo];
+            var userId = _fixture.userIds[userNo];
+            var userLocation = _fixture.userUrls[userNo];
+            var borrowRecordUrl = userLocation + "/tapes/" + tapeId;
 
-            var createRecordResponse = await CreateBorrowRecord(client, borrowRecordUrl);
+            // Create borrow record for user x for tape y (some user and some tape from seeded content intialized)
+            // Expect operation to be successful and return status code of 200 (OK)
+            // Then get the new record as it should be and ensure that an entry exists for user for tape with no return date
+            var createRecordResponse = await client.PostAsync(borrowRecordUrl, null);
             Assert.Equal("", await createRecordResponse.Content.ReadAsStringAsync());
             Assert.Equal(HttpStatusCode.Created, createRecordResponse.StatusCode);
-            var userRecords = (await GetUserBorrowRecords(client, userLocation)).History.ToList();
-            Assert.Single(userRecords);
+            var newRecord = (await GetUserBorrowRecords(userLocation)).History
+                .ToList()
+                .FirstOrDefault(r => r.TapeId == tapeId && r.UserId == userId && r.ReturnDate == null);
+            Assert.NotNull(newRecord);
 
-            // Return the newly created tape
-            var returnTapeResponse = await ReturnTape(client, borrowRecordUrl);
+            // Return the tape we just registered on loan (done with DELETE method)
+            // Verify that operation works (e.g. we recieve status code of 204 (No Content)
+            // Then get all borrow records again and verify that the return date has been set (e.g. is not null anymore)
+            // Then try to return tape again and expect an error (as tape is not on loan for user anymore)
+            var returnTapeResponse = await client.DeleteAsync(borrowRecordUrl);
             Assert.Equal(HttpStatusCode.NoContent, returnTapeResponse.StatusCode);
-
-            // Get all borrow records again and verify that the return date has been updated
-            var allReviews = (await GetUserBorrowRecords(client, userLocation)).History.ToList();
-            Assert.Single(allReviews);
-            Assert.NotNull(allReviews[0].ReturnDate);
-
-            // Try to return tape again to verify that an error occurs
-            returnTapeResponse = await ReturnTape(client, borrowRecordUrl);
+            var review = (await GetUserBorrowRecords(userLocation)).History
+                .ToList()
+                .FirstOrDefault(r => r.TapeId == tapeId && r.UserId == userId);
+            Assert.NotNull(review.ReturnDate);
+            returnTapeResponse =  await client.DeleteAsync(borrowRecordUrl);
             Assert.Equal(HttpStatusCode.NotFound, returnTapeResponse.StatusCode);
 
-            var updateModel = new BorrowRecordInputModel()
-            {
-              BorrowDate = new DateTime(0),
-              ReturnDate = DateTime.Now
-            };
-            var updateReviewResponse = await UpdateBorrowRecord(client, borrowRecordUrl, updateModel);
+            // Update the borrow record (admin functionality only, so full flexibility for input model)
+            // Expect operation to work and get a response of 204 (No Content)
+            // Re-fetch borrow record and expect returned date to be changed
+            var updateModel = new BorrowRecordInputModel(){ BorrowDate = new DateTime(0), ReturnDate = DateTime.Now };
+            var updateReviewResponse = await UpdateBorrowRecord(borrowRecordUrl, updateModel);
             Assert.Equal(HttpStatusCode.NoContent, updateReviewResponse.StatusCode);
-
-            var updatedReviews = (await GetUserBorrowRecords(client, userLocation)).History.ToList();
-            Assert.Single(updatedReviews);
-            Assert.NotEqual(allReviews[0].ReturnDate, updatedReviews[0].ReturnDate);
+            var updatedReview = (await GetUserBorrowRecords(userLocation)).History
+                .ToList()
+                .FirstOrDefault(r => r.TapeId == tapeId && r.UserId == userId);
+            Assert.NotEqual(review.ReturnDate, updatedReview.ReturnDate);
         }
 
         /// <summary>
-        /// Creates new tape into the system e.g. conducts POST request
-        /// Returns response for post request
+        /// Updates borrow record
         /// </summary>
-        /// <param name="client">http client to use to issue request to API</param>
-        /// <param name="url">url to issue request to</param>
-        /// <param name="tapeInput">input model to use to create new tape</param>
-        /// <returns></returns>
-        private async Task<HttpResponseMessage> PostTape(HttpClient client, string url, TapeInputModel tapeInput)
-        {
-            var tapeInputJSON = JsonConvert.SerializeObject(tapeInput);
-            HttpContent content = new StringContent(tapeInputJSON, Encoding.UTF8, "application/json");
-            return await client.PostAsync(url, content);
-        }
-
-        /// <summary>
-        /// Creates new user into the system e.g. conducts POST request
-        /// Returns response for post request
-        /// </summary>
-        /// <param name="client">http client to use to issue request to API</param>
-        /// <param name="url">url to issue request to</param>
-        /// <param name="tapeInput">input model to use to create new tape</param>
-        /// <returns></returns>
-        private async Task<HttpResponseMessage> PostUser(HttpClient client, string url, UserInputModel userInput)
-        {
-            var userInputJSON = JsonConvert.SerializeObject(userInput);
-            HttpContent content = new StringContent(userInputJSON, Encoding.UTF8, "application/json");
-            return await client.PostAsync(url, content);
-        }
-
-        private async Task<HttpResponseMessage> CreateBorrowRecord(HttpClient client, string url)
-        {
-            return await client.PostAsync(url, null);
-        }
-
-        private async Task<HttpResponseMessage> GetUrl(HttpClient client, string url)
-        {
-            return await client.GetAsync(url);
-        }
-
-        private async Task<HttpResponseMessage> UpdateBorrowRecord(HttpClient client, string url, BorrowRecordInputModel input)
+        /// <param name="url">url to borrow record</param>
+        /// <param name="input">input model for borrow record</param>
+        /// <returns>the API HTTP response to request</returns>
+        private async Task<HttpResponseMessage> UpdateBorrowRecord(string url, BorrowRecordInputModel input)
         {
             var recordInputJson = JsonConvert.SerializeObject(input);
             HttpContent content = new StringContent(recordInputJson, Encoding.UTF8, "application/json");
             return await client.PutAsync(url, content);
         }
 
-        private async Task<HttpResponseMessage> ReturnTape(HttpClient client, string url)
+        /// <summary>
+        /// Gets list of all borrow records for user
+        /// </summary>
+        /// <param name="url">url to user borrow records</param>
+        /// <returns>the API HTTP response to request</returns>
+        private async Task<UserDetailDTO> GetUserBorrowRecords(string url)
         {
-            return await client.DeleteAsync(url);
-        }
-
-        private async Task<UserDetailDTO> GetUserBorrowRecords(HttpClient client, string url)
-        {
-            var getUserReviewsResponse = await GetUrl(client, url);
+            var getUserReviewsResponse = await client.GetAsync(url);
             Assert.Equal(HttpStatusCode.OK, getUserReviewsResponse.StatusCode);
             var borrowRecords = JsonConvert.DeserializeObject<UserDetailDTO>(await getUserReviewsResponse.Content.ReadAsStringAsync());
             return borrowRecords;
-        }
-
-        private string GetRecordPath(string userpath, int tapeId)
-        {
-            return userpath + "/tapes/" + tapeId;
         }
     }
 }
